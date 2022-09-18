536325250e9d7fd33ff288c948ccfe597ff000a2
Kai Franke <cosinushh@gmail.com>
2022-09-18 21:45:10 +0200

    remove useless variable in index.js

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]',
 );
-// const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
 
 // States
